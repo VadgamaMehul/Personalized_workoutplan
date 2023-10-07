a5d258f0736e3b95a7dd132658f279146d5864d0
Mehul Vadgama <vadgama.mehul444@gmail.com>
2023-10-07 17:43:41 +0530

    Added Project To Repo

--- Mehul's Project/src/Utils/t.js
+++
@@ -1,7 +0,0 @@
-// const data = require("./dummy.json");
-
-// const ar = data.filter((e)=>{
-//     return e.gender.includes('male');
-// })
-
-// console.log(ar);
--- Mehul's Project/src/Utils/test.mjs
+++
@@ -1,8 +0,0 @@
-// import dummy from "./dummy.json"
-
-// let arr = dummy.filter((e) => {
-//   return (
-//     e.age?.includes("male") );
-// });
-
-// console.log(dummy);
