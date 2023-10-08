a77dbde368a010a0e8d53e6fa271050a06743fcc
Mehul Vadgama <vadgama.mehul444@gmail.com>
2023-10-08 13:42:26 +0530

    Added Project To Repo

--- src/Redux/dataSlice.js
+++ src/Redux/dataSlice.js
@@ -1,64 +1,77 @@
-import { createSlice } from '@reduxjs/toolkit';
-import data from '../Utils/dummy.json'; // Assuming you have a valid JSON file at this path
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../Utils/dummy.json"; // Assuming you have a valid JSON file at this path
 
 const initialState = {
-	name: '',
-	gender: '',
-	age: '',
-	height: '',
-	weight: '',
-	body_type: '',
-	workoutex: '',
-	data: [],
+  name: "",
+  gender: "",
+  age: "",
+  height: "",
+  weight: "",
+  body_type: "",
+  workoutex: "",
+  data: [],
 };
 
 const dataSlice = createSlice({
-	name: 'data',
-	initialState,
-	reducers: {
-		addAge(state, { payload }) {
-			state.age = payload;
-			console.log(payload);
-		},
-		addGender(state, { payload }) {
-			state.gender = payload;
-			console.log(payload);
-		},
-		addName(state, { payload }) {
-			state.name = payload;
-			console.log(payload);
-		},
-		addHeight(state, { payload }) {
-			state.height = payload;
-		},
-		addWeight(state, { payload }) {
-			state.weight = payload;
-		},
-		addBodytype(state, { payload }) {
-			state.bodytype = payload;
-		},
-		addWorkoutex(state, { payload }) {
-			state.workoutex = payload;
-		},
-		getPlan(state, action) {
-			const {gender, age, height, weight, bodytype, workoutex } = action.payload;
+  name: "data",
+  initialState,
+  reducers: {
+    addAge(state, { payload }) {
+      state.age = payload;
+      console.log(payload);
+    },
+    addGender(state, { payload }) {
+      state.gender = payload;
+      console.log(payload);
+    },
+    addName(state, { payload }) {
+      state.name = payload;
+      console.log(payload);
+    },
+    addHeight(state, { payload }) {
+      state.height = payload;
+    },
+    addWeight(state, { payload }) {
+      state.weight = payload;
+    },
+    addBodytype(state, { payload }) {
+      state.bodytype = payload;
+    },
+    addWorkoutex(state, { payload }) {
+      state.workoutex = payload;
+    },
+    getPlan(state, action) {
+      const { gender, age, height, weight, bodytype, workoutex } =
+        action.payload;
 
-			const res = data.filter((ele) => {
-				return (
-					ele.age === parseInt(age) &&
-					ele.gender === gender &&
-					ele.body_type === bodytype &&
-					ele.weight === parseInt(weight) &&
-					ele.height === height &&
-					ele.previous_workout_experience === workoutex
-				);
-			});
+      const res = data.filter((ele) => {
+        return (
+          // ele.age === parseInt(age) &&
+          // ele.gender === gender &&
+          // ele.body_type === bodytype &&
+          // ele.weight === parseInt(weight) &&
+          // ele.height === height &&
+          // ele.previous_workout_experience === workoutex
+          ele.gender.includes(gender) &&
+          ele.body_type.includes(bodytype) &&
+          ele.previous_workout_experience.includes(workoutex)
+        );
+      });
 
-			state.data = res;
-		},
-	},
+      state.data = res;
+    },
+  },
 });
 
-export const { addAge, addName, addGender, addHeight, addWeight, addBodytype, addWorkoutex, getPlan } = dataSlice.actions;
+export const {
+  addAge,
+  addName,
+  addGender,
+  addHeight,
+  addWeight,
+  addBodytype,
+  addWorkoutex,
+  getPlan,
+} = dataSlice.actions;
 
 export default dataSlice.reducer;
